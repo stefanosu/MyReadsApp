29946477f6894b432a018d0f08baba5b073b24ee
Stefanos <stefanosu422@gmail.com>
2018-11-01 21:22:48 -0400

    Delete FAB.js

--- src/components/FAB.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export default class FAB extends Component {
-	render(){
-		return( <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>)
-	}
-}	
